f9125525d1b52067ed4b6c1eebcad2b215696b86
jaguarmayan@protonmail.com <jrtejada.estrada@gmail.com>
2022-12-08 09:07:10 -0600

    First Commit

---
+++ gitpush.sh
@@ -0,0 +1,18 @@
+
+
+#! /bin/sh
+#
+echo "Enter Message"
+read message
+
+git add * 
+git commit -m "${message}"
+if [-n "$(git status - porcelain)"];
+then
+	echo "IT's CLEAN"
+else
+	git status
+	echo "Pushing data to remote server!!!"
+	git push 
+fi
+
